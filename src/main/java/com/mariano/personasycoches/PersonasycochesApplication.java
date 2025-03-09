0be2cee343314bd3012510b30e1f48aae497d03f
Mariano <mariano.ramaidana@gmail.com>
2025-03-09 20:49:39 -0300

    primer commit, creación del proyecto

---
+++ src/main/java/com/mariano/personasycoches/PersonasycochesApplication.java
@@ -0,0 +1,13 @@
+package com.mariano.personasycoches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonasycochesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonasycochesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mariano/personasycoches/PersonasycochesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariano.personasycoches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonasycochesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
